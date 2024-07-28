b8ff28db1d5a59dc806ebfcb0bca4dbc25a9cbdf
Ryster <ry.diffusion@proton.me>
2024-07-28 16:04:16 -0300

    update

--- confs/fish/config.fish
+++ confs/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/ryster/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
