c7ff0d6129877af5c3ddad4c644b0d28a351d7b4
Ryster <ry.diffusion@proton.me>
2024-09-07 16:53:52 -0300

    hyprrrrrrrrrrr

--- config.py
+++ config.py
@@ -10,25 +10,30 @@ PROFILES = {
         Link("helix", "confs/helix", "~/.config/helix"),
         Link("git", "confs/gitconfig", "~/.gitconfig"),
     ],
-    "ubuntu": [
+    "archLinux": [
         Link("neovim", "confs/neovim", "~/.config/nvim"),
         Link("fish", "confs/fish", "~/.config/fish"),
         Link("helix", "confs/helix", "~/.config/helix"),
         Link("git", "confs/gitconfig-linux", "~/.gitconfig"),
         Link("cargo", "confs/cargo-config.toml", "~/.cargo/config.toml"),
+        Link("hypr", "confs/hypr", "~/.config/hypr"),
         # SudoCopy("paru", "confs/paru.conf", "/etc/paru.conf"),
     ],
     "Windows": [
         EnsureDirectory(KOMORBI_CONFIG_HOME),
-        Link("komorebi", "confs/win/komorebi.json", f"{KOMORBI_CONFIG_HOME}/komorebi.json"),
-        Link("komorebi-ahk", "confs/win/komorebi.ahk", f"{KOMORBI_CONFIG_HOME}/komorebi.ahk"),
-        Link("kanata", "confs/kanata.kbd", f"{MISC_CONFIG_HOME}/kanata.kbd"),   
-        InstallPwshProfile("win-pwsh-profile",
-            "confs/win/profile.ps1"
+        Link(
+            "komorebi",
+            "confs/win/komorebi.json",
+            f"{KOMORBI_CONFIG_HOME}/komorebi.json",
         ),
-
+        Link(
+            "komorebi-ahk",
+            "confs/win/komorebi.ahk",
+            f"{KOMORBI_CONFIG_HOME}/komorebi.ahk",
+        ),
+        Link("kanata", "confs/kanata.kbd", f"{MISC_CONFIG_HOME}/kanata.kbd"),
+        InstallPwshProfile("win-pwsh-profile", "confs/win/profile.ps1"),
         Link("git", "confs/gitconfig", "~/.gitconfig"),
         Powershell("git config --global gpg.program (Get-Command gpg).Source"),
-
     ],
 }
