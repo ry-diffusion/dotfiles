2c218c5e50d5a7b6986c22249d673fbe9dec11c8
Ryster <ry.diffusion@proton.me>
2025-01-09 17:49:26 -0300

    here we go

--- confs/neovim/lua/config/options.lua
+++ confs/neovim/lua/config/options.lua
@@ -1,2 +1,3 @@
-vim.o.guifont = "Intel One Mono:h11"
-vim.g.neovide_cursor_vfx_mode = "railgun"
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
